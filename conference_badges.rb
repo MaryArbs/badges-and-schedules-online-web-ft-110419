717cd54deaae391818828a92ad386e38769f8209
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-06 20:24:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,4 +15,13 @@ def assign_rooms (array)
   array. each_with_index { |array,index| new_array.push ("Hello, #{array}! You'll be assigned to room #{index+1}!") }
   return new_array
 end
+
 def printer (array)
+  batch_badge_creator(array). each do |batch_badge_creator|
+    puts batch_badge_creator
+  end
+  assign_rooms(array).each do |assign_rooms|
+    puts assign_rooms
+  end 
+end 
+
