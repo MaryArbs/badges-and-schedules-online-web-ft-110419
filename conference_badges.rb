3de5ae20f939bf369c7a4b2294021f8c97007f2b
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-06 19:16:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+attendees= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  new_array=[]
+  attendees. each do |name|
+    puts "Hello, my name is #{name}."
+  end
+  new_array
+end
